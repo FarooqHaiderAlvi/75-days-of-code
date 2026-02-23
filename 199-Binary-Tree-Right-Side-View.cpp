cde7f8590d80262dfd81400a4fc7e6414eec02f8
Farooq Haider Alvi <farooqhaideralvi1999@gmail.com>
2026-02-24 04:16:42 +0500

    199. Binary Tree Right Side View

---
+++ 199-Binary-Tree-Right-Side-View.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+
+        vector<int> res;
+        if (!root)
+            return res;
+
+        queue<TreeNode*> q;
+        q.push(root);
+        int depth = 0;
+
+        while (!q.empty()) {
+            int levelSize = q.size();
+
+            // Traverse all nodes at the current level
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode* curr = q.front();
+                q.pop();
+                if (i == levelSize - 1)
+                    res.push_back(curr->val);
+
+                if (curr->left)
+                    q.push(curr->left);
+                if (curr->right)
+                    q.push(curr->right);
+            }
+        }
+        return res;
+    }
+};
